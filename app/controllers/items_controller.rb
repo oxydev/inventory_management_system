b6d5a24705fdcbc3aa7ba25356f77aeeb115dc3a
Shrijal <tamrakarshrijal@gmail.com>
2019-07-23 15:08:53 +0545

    added create edit and show asset

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  
   def index
     @items = Item.all
   end
@@ -9,7 +10,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    
+    @item.asset_code = get_asset_code(@item)
     if @item.save
       redirect_to items_path
     else
@@ -17,16 +18,41 @@ class ItemsController < ApplicationController
     end
   end
 
-  def edit
-
+  def show
+    @item = Item.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update_attributes(item_params)
+      redirect_to(item_path(@item))
+    else
+      render('edit')
+    end
+  end
+
 
   def delete
+    @item = Item.find(params[:id])
   end
+
+  def destroy
+  end
+
 
   private
 
   def item_params
-    params.require(:item).permit(:asset, :asset_code, :date_of_purchase)
+    params.require(:item).permit(:asset, :date_of_purchase)
+  end
+
+  def get_asset_code(item)
+    date = item.date_of_purchase
+    code = item.asset + date.year.to_s + date.month.to_s + date.day.to_s 
   end
 end
