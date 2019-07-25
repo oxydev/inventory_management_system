00a238456e975373618ef002b95d08e7d7dfd7ff
Shrijal <tamrakarshrijal@gmail.com>
2019-07-25 09:13:52 +0545

    mailer use_request  skeleton

--- app/mailers/asset_mailer.rb
+++ app/mailers/asset_mailer.rb
@@ -11,4 +11,11 @@ class AssetMailer < ApplicationMailer
     @user = @purchase.user
     mail(to: @user.email, subject: 'Respone for the Request of Asset Purchase.')
   end
+
+  def use_request
+  end
+
+  def use_response
+  end
+  
 end
